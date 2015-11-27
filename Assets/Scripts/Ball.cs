231e477cd6b3d3839238b2a08c0b00fa6a0ff12d
Luc Tremblay <luc@tons-of-slack.net>
2015-11-27 17:44:44 -0500

    first incomplete implementation

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody rigidBody;
+
+    Vector3 ballOrigin;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    rigidBody = GetComponent<Rigidbody>();
+	    ballOrigin = transform.position; // store the original position of the ball so we reset at that place
+
+	    ResetBall();
+	}
+
+    void ResetBall()
+    {
+        transform.position = ballOrigin;
+        rigidBody.velocity = new Vector3(Random.value, 0, Random.value).normalized * 50;
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        
+    }
+}
